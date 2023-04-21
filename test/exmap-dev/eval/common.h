0337668517cd79abbdba312891d556293d8fc712
Aaron-Olt <113511267+Aaron-Olt@users.noreply.github.com>
2023-04-21 10:39:19 +0200

    Delete test directory

--- test/exmap-dev/eval/common.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#define die(fmt) do { perror(fmt); exit(EXIT_FAILURE); } while(0)
-#define ARRAY_SIZE(arr) (sizeof(arr)/sizeof(*arr))
-
-const unsigned pageSize = 4096;
-
-int open_file(const char *fn, size_t *file_size);
--- test/readme.txt
+++
@@ -1,1 +0,0 @@
-  Sudo ./test_script.sh startet das test bench, eventuell muss es noch mit chmod +x ausführbar gemacht werden. Die Ergebnisse landen im tests Ordner.
